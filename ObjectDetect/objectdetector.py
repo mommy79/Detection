9d69a8141a12f2aefdac675e7b8286777486b4fa
SeoPaul <seopaul@SeoPaului-MacBook-Pro.local>
2018-08-18 21:58:05 +0900

    [Feature] Modify Coding Style

--- ObjectDetect/objectdetector.py
+++ ObjectDetect/objectdetector.py
@@ -2,127 +2,77 @@
 # -*-coding:utf-8-*-
 import numpy as np
 import time
-import os
 import cv2
+import os
 
 
 class ObjectDetector:
     def __init__(self):
         # 이미지를 불러오는 경로에 관한 변수
-        self.__path = os.getcwd() + "/data"
+        self.__path = None
 
         # 영상에서 찾고자하는 물체이미지를 저장한 변수
-        self.__obj_image = dict(traffic=cv2.imread(self.__path + "/card1.jpg"),
-                                parking=cv2.imread(self.__path + "/card2.jpg"),
-                                crossbar=cv2.imread(self.__path + "/card3.jpg"),
-                                tunnel=cv2.imread(self.__path + "/card4.jpg"))
+        self.__key = list()
+        self.__cmp_image = dict()
 
         # 물체이미지에 관한 정보를 담는 변수
-        self.__kp = dict(kp1=[], kp2=[], kp3=[], kp4=[])
-        self.__des = dict(des1=[], des2=[], des3=[], des4=[])
+        self.__kp = dict()
+        self.__des = dict()
 
         # 물체이미지의 특징점을 미리 계산해
-        self.flann = self._set_tools()
+        self.__sift = cv2.xfeatures2d.SIFT_create()
+        self.__flann = None
 
-    # 물체인식에 필요한 도구를 만드는 메소드
-    def _set_tools(self):
-        sift = cv2.xfeatures2d.SIFT_create()
-        for key in self.__obj_image:
-            if key == 'traffic':
-                self.__kp['kp1'], self.__des['des1'] = sift.detectAndCompute(self.__obj_image[key], None)
-            elif key == 'parking':
-                self.__kp['kp2'], self.__des['des2'] = sift.detectAndCompute(self.__obj_image[key], None)
-            elif key == 'crossbar':
-                self.__kp['kp3'], self.__des['des3'] = sift.detectAndCompute(self.__obj_image[key], None)
-            elif key == 'tunnel':
-                self.__kp['kp4'], self.__des['des4'] = sift.detectAndCompute(self.__obj_image[key], None)
-            else:
-                return
+    # 물체이미지가 있는 디렉토리 경로를 설정하는 메소드
+    def set_path(self, path):
+        self.__path = path
 
+    # 물체이미지의 데이터와 이름을 설정하는 메소드
+    def set_cmp_image(self, key):
+        if len(key) == 0:
+            raise ValueError
+
+        self.__key = key
+        for k in key:
+            img = cv2.imread(self.__path + "/" + k[0] + ".jpg")
+            if img is None:
+                print("[WARNING] " + k[0] + ".png not found!")
+                continue
+            self.__cmp_image.update({k[0]: img})
+
+    # 물체인식에 필요한 비교이미지의 특징점을 구하는 메소드
+    def set_initialize(self):
+        for key in self.__key:
+            self.__kp[key[0]], self.__des[key[0]] = self.__sift.detectAndCompute(self.__cmp_image[key[0]], None)
         index_params = dict(algorithm=0, trees=5)
         search_params = dict(checks=50)
-        return cv2.FlannBasedMatcher(index_params, search_params)
+        self.__flann = cv2.FlannBasedMatcher(index_params, search_params)
 
     def __call__(self, src, fps):
-        sift = cv2.xfeatures2d.SIFT_create()
-        src_kp, src_des = sift.detectAndCompute(src, None)
+        src_kp, src_des = self.__sift.detectAndCompute(src, None)
 
         matches = dict()
-        for key in self.__obj_image:
-            if key == 'traffic':
-                matches.update({'match1': self.flann.knnMatch(src_des, self.__des['des1'], k=2)})
-            elif key == 'parking':
-                matches.update({'match2': self.flann.knnMatch(src_des, self.__des['des2'], k=2)})
-            elif key == 'crossbar':
-                matches.update({'match3': self.flann.knnMatch(src_des, self.__des['des3'], k=2)})
-            elif key == 'tunnel':
-                matches.update({'match4': self.flann.knnMatch(src_des, self.__des['des4'], k=2)})
-            else:
-                return
+        for key in self.__key:
+            matches.update({key[0]: self.__flann.knnMatch(src_des, self.__des[key[0]], k=2)})
 
-        good = dict(good1=[], good2=[], good3=[], good4=[])
-        for key in matches:
-            for m, n in matches[key]:
-                if m.distance < 0.55 * n.distance:
-                    if key == 'match1':
-                        good['good1'].append(m)
-                    elif key == 'match2':
-                        good['good2'].append(m)
-                    elif key == 'match3':
-                        good['good3'].append(m)
-                    elif key == 'match4':
-                        good['good4'].append(m)
-                    else:
-                        return
+        good = dict()
+        for key in self.__key:
+            good.update({key[0]: []})
+            good[key[0]] = [m for m, n in matches[key[0]] if m.distance < 0.6 * n.distance]
 
         result = src
-        if len(good['good1']) > 7:
-            src_pts = np.float32([src_kp[m.queryIdx].pt for m in good['good1']]).reshape(-1, 1, 2)
-            dst_pts = np.float32([self.__kp['kp1'][m.trainIdx].pt for m in good['good1']]).reshape(-1, 1, 2)
+        for key in self.__key:
+            if len(good[key[0]]) > key[1]:
+                src_pts = np.float32([src_kp[m.queryIdx].pt for m in good[key[0]]]).reshape(-1, 1, 2)
+                dst_pts = np.float32([self.__kp[key[0]][m.trainIdx].pt for m in good[key[0]]]).reshape(-1, 1, 2)
 
-            sum = np.sum((src_pts - dst_pts) ** 2)
-            num_all = src_pts.shape[0] * src_pts.shape[1]
-            mse = sum / num_all
-            if mse < 40000:
-                print("Traffic Sign Detect")
-                result = cv2.drawMatches(src, src_kp, self.__obj_image['traffic'], self.__kp['kp1'],
-                                         good['good1'], None, flags=2)
-
-        elif len(good['good2']) > 7:
-            src_pts = np.float32([src_kp[m.queryIdx].pt for m in good['good2']]).reshape(-1, 1, 2)
-            dst_pts = np.float32([self.__kp['kp2'][m.trainIdx].pt for m in good['good2']]).reshape(-1, 1, 2)
-
-            sum = np.sum((src_pts - dst_pts) ** 2)
-            num_all = src_pts.shape[0] * src_pts.shape[1]
-            mse = sum / num_all
-            if mse < 40000:
-                print("Parking Sign Detect")
-                result = cv2.drawMatches(src, src_kp, self.__obj_image['parking'], self.__kp['kp2'],
-                                         good['good2'], None, flags=2)
-
-        elif len(good['good3']) > 7:
-            src_pts = np.float32([src_kp[m.queryIdx].pt for m in good['good3']]).reshape(-1, 1, 2)
-            dst_pts = np.float32([self.__kp['kp3'][m.trainIdx].pt for m in good['good3']]).reshape(-1, 1, 2)
-
-            sum = np.sum((src_pts - dst_pts) ** 2)
-            num_all = src_pts.shape[0] * src_pts.shape[1]
-            mse = sum / num_all
-            if mse < 40000:
-                print("Crossbar Detect")
-                result = cv2.drawMatches(src, src_kp, self.__obj_image['crossbar'], self.__kp['kp3'],
-                                         good['good3'], None, flags=2)
-
-        elif len(good['good4']) > 7:
-            src_pts = np.float32([src_kp[m.queryIdx].pt for m in good['good4']]).reshape(-1, 1, 2)
-            dst_pts = np.float32([self.__kp['kp4'][m.trainIdx].pt for m in good['good4']]).reshape(-1, 1, 2)
-
-            sum = np.sum((src_pts - dst_pts) ** 2)
-            num_all = src_pts.shape[0] * src_pts.shape[1]
-            mse = sum / num_all
-            if mse < 40000:
-                print("Tunnel Sign Detect")
-                result = cv2.drawMatches(src, src_kp, self.__obj_image['tunnel'], self.__kp['kp4'],
-                                         good['good4'], None, flags=2)
+                sum = np.sum((src_pts - dst_pts) ** 2)
+                num_all = src_pts.shape[0] * src_pts.shape[1]
+                mse = sum / num_all
+                if mse < key[2]:
+                    print(key[0][0].upper() + key[0][1:] + " Sign Detect")
+                    result = cv2.drawMatches(src, src_kp, self.__cmp_image[key[0]], self.__kp[key[0]],
+                                             good[key[0]], None, flags=2)
         fps = "FPS : %0.1f" % fps
         result = cv2.putText(result, fps, (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         return result
@@ -130,12 +80,19 @@ class ObjectDetector:
 
 if __name__ == '__main__':
     obj = ObjectDetector()
-    cap = cv2.VideoCapture(0)
+    obj.set_path(os.getcwd() + "/data")
 
+    # k[0] : Image Name
+    k = [['parking', 4, 20000]]
+
+    obj.set_cmp_image(k)
+    obj.set_initialize()
+
+    cap = cv2.VideoCapture(0)
     prev_time = 0
     while cap.isOpened():
         ret, fm = cap.read()
-        fm = cv2.resize(fm, (480, 240))
+        fm = cv2.resize(fm, (640, 240))
 
         # FPS 를 구하는 과정
         cur_time = time.time()
@@ -152,3 +109,4 @@ if __name__ == '__main__':
                 break
     cap.release()
     cv2.destroyAllWindows()
+
